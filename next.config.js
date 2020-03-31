181e2b8cd81f01a6ec7d3b884983892b7a7b6f0c
Алейдаров Эльдар <ldrv5555@gmail.com>
2020-03-31 20:17:48 +0300

    signal-client, signal-server, sockets, custom server, downgrade zeit now to v1

--- next.config.js
+++ next.config.js
@@ -8,34 +8,6 @@ const {
   mp3Loader
 } = require('./webpack.config');
 
-const nextOfflineConfig = {
-  target: 'serverless',
-  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-  // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: true,
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
-};
-
 module.exports = withOffline({
   webpack(config, { isServer }) {
     const { rules } = config.module;
@@ -58,6 +30,5 @@ module.exports = withOffline({
     });
 
     return merge(config, cfg);
-  },
-  ...nextOfflineConfig
+  }
 });
