b9c6421ce44aec857ece347fd66133803ec2836b
Алейдаров Эльдар <ldrv5555@gmail.com>
2020-04-14 11:58:26 +0300

    markup

--- components/VideoChat.jsx
+++ components/VideoChat.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable no-alert */
 import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import io from 'socket.io-client';
 import SimpleSignalClient from 'simple-signal-client';
 
+import { IconButton, Slider } from '@material-ui/core';
+import { VolumeDown, VolumeUp } from '@material-ui/icons';
+
+import { PhoneIcon, MicrophoneIcon } from 'public/icons';
+
 import StreamVideo from './StreamVideo';
-import Layout from './Layout';
+import TopBar from './TopBar';
 
 const isDev = process.env.NODE_ENV !== 'production';
 
@@ -26,10 +32,18 @@ const getLocalStream = async () =>
       return getLocalStream();
     });
 
-const VideoChat = ({ currentRoom, client, setCurrentRoom }) => {
-  const [signalClient] = useState(() => client || initSignalClient());
+const VideoChat = ({ currentRoom, setCurrentRoom }) => {
+  const [signalClient] = useState(() => initSignalClient());
   const [localStream, setLocalStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
+
+  const [value, setValue] = useState(50);
+  const onChange = (event, newValue) => setValue(newValue);
+
+  const [muted, setMuted] = useState(false);
+  const toggleMuted = () => setMuted(!muted);
+
+  const isDoctor = !!setCurrentRoom;
 
   const connectToPeer = useCallback(
     peer => {
@@ -42,6 +56,9 @@ const VideoChat = ({ currentRoom, client, setCurrentRoom }) => {
 
   useEffect(() => {
     getLocalStream().then(stream => setLocalStream(stream));
+    if (isDoctor) {
+      signalClient.discover(null);
+    }
   }, []);
 
   useEffect(() => {
@@ -58,7 +75,7 @@ const VideoChat = ({ currentRoom, client, setCurrentRoom }) => {
           .then(({ peer }) => connectToPeer(peer))
           .catch(error => {
             if (error) {
-              if (setCurrentRoom) {
+              if (isDoctor) {
                 signalClient.discover({ remove: currentRoom });
                 setCurrentRoom(null);
               }
@@ -69,17 +86,130 @@ const VideoChat = ({ currentRoom, client, setCurrentRoom }) => {
   }, [localStream]);
 
   return (
-    <Layout>
-      <StreamVideo muted autoPlay srcObject={localStream} />
-      {remoteStream && <StreamVideo muted autoPlay srcObject={remoteStream} />}
-    </Layout>
+    <Container>
+      <TopBar />
+      <VideoChatContainer>
+        <VideoContainer>
+          <Name>{isDoctor ? currentRoom : 'Сергеев П.В.'}</Name>
+          {remoteStream ? (
+            <RemoteVideo muted autoPlay srcObject={remoteStream} />
+          ) : (
+            <LoaderContainer />
+          )}
+          <LocalVideo muted autoPlay srcObject={localStream} />
+          <ToolBar>
+            <div>
+              <SliderContainer>
+                <VolumeDown />
+                <Slider value={value} onChange={onChange} />
+                <VolumeUp />
+              </SliderContainer>
+              <IconButtonStyled onClick={toggleMuted}>
+                <MicrophoneIcon />
+              </IconButtonStyled>
+              <DropIconButton onClick={() => setCurrentRoom()}>
+                <PhoneIcon />
+              </DropIconButton>
+            </div>
+          </ToolBar>
+        </VideoContainer>
+      </VideoChatContainer>
+    </Container>
   );
 };
 
 VideoChat.propTypes = {
   currentRoom: PropTypes.string,
-  client: PropTypes.object,
   setCurrentRoom: PropTypes.func
 };
 
 export default VideoChat;
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const VideoChatContainer = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const VideoContainer = styled.div`
+  position: relative;
+  height: 100%;
+  width: min-content;
+`;
+
+const LoaderContainer = styled.video`
+  width: 60vw;
+  height: 100%;
+  background: ${({ theme }) => theme.palette.gray.main};
+`;
+
+const RemoteVideo = styled(StreamVideo)`
+  height: 100%;
+`;
+
+const Name = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 4px 8px;
+  border-radius: 24px;
+  margin: 24px;
+
+  background: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) => theme.palette.primary.contrastText};
+`;
+
+const LocalVideo = styled(StreamVideo)`
+  position: absolute;
+  right: 24px;
+  bottom: 24px;
+  width: 200px;
+`;
+
+// ---------
+
+const ToolBar = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+
+  left: 0;
+  right: 0;
+  bottom: 24px;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 8px;
+  }
+`;
+
+const IconButtonStyled = styled(IconButton)`
+  margin: 12px;
+  border: 1px solid white;
+`;
+
+const DropIconButton = styled(IconButton)`
+  margin: 12px;
+  border: 1px solid white;
+  background: rgba(255, 0, 0, 0.25);
+`;
+
+const SliderContainer = styled.div`
+  display: flex;
+  width: 140px;
+  border: 1px solid white;
+  padding: 12px;
+  border-radius: 100px;
+  align-items: center;
+
+  svg {
+    margin: 4px;
+  }
+`;
